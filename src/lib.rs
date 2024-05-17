4db471b3007142b948112a7fd76ee090bff0a5ce
kopf1 <github@ryankopf.com>
2024-05-17 02:19:06 -0500

    chore: Bump version to 0.1.1 and add default replacements in mvc_views attribute

--- src/lib.rs
+++ src/lib.rs
@@ -2,62 +2,63 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, ItemMod, Stmt};
+use syn::{parse_macro_input, ItemMod, File, Item, Stmt};
 
 #[proc_macro_attribute]
 pub fn mvc_views(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(item as ItemMod);
-    let mod_name = input.ident.to_string();
+    let input = parse_macro_input!(item as File);
 
-    let mut output = input.clone();
     let mut new_items = vec![];
 
-    if let Some((brace, items)) = input.content {
-        for item in items {
-            if let syn::Item::Fn(mut func) = item {
-                let name = func.sig.ident.to_string();
+    for item in input.items {
+        if let Item::Fn(mut func) = item {
+            let name = func.sig.ident.to_string();
 
-                // Inject a default params definition at the start of the function
-                let default_params: Stmt = syn::parse_quote! {
-                    let params: Option<HashMap<String, String>> = None;
-                };
-                func.block.stmts.insert(0, default_params);
+            // Inject a default params definition at the start of the function
+            let default_replacements: Stmt = syn::parse_quote! {
+                let replacements: Option<std::collections::HashMap<String, String>> = None;
+                // let mut replacements = HashMap::new();
+            };
+            func.block.stmts.insert(0, default_replacements);
 
-                // Check if the function already ends with a value
-                let last_stmt = func.block.stmts.last();
-                let ends_with_value = match last_stmt {
-                    Some(syn::Stmt::Expr(_)) => true,
-                    Some(syn::Stmt::Semi(expr, _)) => match *expr {
-                        syn::Expr::Return(_) => true,
-                        syn::Expr::Block(_) => true,
-                        syn::Expr::Call(_) => true,
-                        _ => false,
-                    },
+            // Check if the function already ends with a value
+            let last_stmt = func.block.stmts.last();
+            let ends_with_value = match last_stmt {
+                Some(syn::Stmt::Expr(_)) => true,
+                Some(syn::Stmt::Semi(expr, _)) => match *expr {
+                    syn::Expr::Return(_) => true,
+                    syn::Expr::Block(_) => true,
+                    syn::Expr::Call(_) => true,
                     _ => false,
+                },
+                _ => false,
+            };
+
+            if !ends_with_value {
+                let block: Stmt = syn::parse_quote! {
+                    {
+                        let form_path = format!("./src/views/emails/{}.html", #name);
+                        let content = std::fs::read_to_string(&form_path).unwrap_or_default();
+                        let content = crate::renderer::render(content, &host, replacements.into());
+                        return HttpResponse::Ok()
+                            .content_type("text/html")
+                            .body(content);
+                    }
                 };
-
-                if !ends_with_value {
-                    let block: Stmt = syn::parse_quote! {
-                        {
-                            let form_path = format!("./src/views/{}/{}.html", #mod_name, #name);
-                            let content = std::fs::read_to_string(&form_path).unwrap_or_default();
-                            let content = crate::renderer::render(content, &host, params.as_ref());
-                            return HttpResponse::Ok()
-                                .content_type("text/html")
-                                .body(content);
-                        }
-                    };
-                    func.block.stmts.push(block);
-                }
-
-                new_items.push(syn::Item::Fn(func));
-            } else {
-                new_items.push(item);
+                func.block.stmts.push(block);
             }
-        }
 
-        output.content = Some((brace, new_items));
+            new_items.push(Item::Fn(func));
+        } else {
+            new_items.push(item);
+        }
     }
+
+    let output = File {
+        shebang: input.shebang,
+        attrs: input.attrs,
+        items: new_items,
+    };
 
     TokenStream::from(quote!(#output))
 }
