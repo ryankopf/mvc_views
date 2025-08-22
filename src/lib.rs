f4b2ffe6ae95a6aa3b3ca2d7c7e9ab6545887f9d
Ryan Kopf <github@ryankopf.com>
2025-08-21 23:31:40 -0500

    Remove println

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,6 @@ fn rewrite_fn(mut func: ItemFn) -> Item {
             let view_name = stringify!(#ident);
             let form_path = format!("./src/views/{}/{}.html", controller, view_name);
             let content = std::fs::read_to_string(&form_path).unwrap_or_default();
-            println!("Replacements: {:?}", #replacement_hashmap);
             let content = crate::renderer::render(content, &host, #replacement_hashmap);
             return HttpResponse::Ok().content_type("text/html").body(content);
         }};
